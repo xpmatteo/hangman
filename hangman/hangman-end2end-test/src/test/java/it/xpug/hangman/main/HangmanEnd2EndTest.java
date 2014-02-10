ea53c0c88a1f3679e548cd7703e2f7e400c2cf7e
Matteo Vaccari <vaccari@pobox.com>
2014-02-10 18:51:52 +0100

    First tests pass

--- hangman/hangman-end2end-test/src/test/java/it/xpug/hangman/main/HangmanEnd2EndTest.java
+++ hangman/hangman-end2end-test/src/test/java/it/xpug/hangman/main/HangmanEnd2EndTest.java
@@ -1,17 +1,50 @@
 package it.xpug.hangman.main;
 
 
+import static org.apache.commons.io.IOUtils.*;
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 import it.xpug.generic.web.*;
 
+import org.apache.commons.io.*;
+import org.apache.http.*;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
 import org.junit.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.*;
 
 public class HangmanEnd2EndTest {
 
-	private static final int APPLICATION_PORT = 8123;
-	protected static ReusableJettyApp app = new ReusableJettyApp(new HangmanServlet());
-	protected static WebDriver driver = new FirefoxDriver();
+	@Test
+	public void rootUrl() throws Exception {
+		get("/");
+		assertStatus(200);
+		assertBody("{}");
+		assertMimeType("application/json; charset=UTF-8");
+	}
+
+	private void assertBody(String expectedBody) throws IllegalStateException, IOException {
+		List<String> body = readLines(response.getEntity().getContent());
+		assertEquals("Body", expectedBody, body.get(0));
+	}
+
+	private void assertMimeType(String expectedMimeType) {
+		assertEquals("Mime type", expectedMimeType, response.getLastHeader("content-type").getValue());
+	}
+
+	private void assertStatus(int expectedStatus) {
+		assertEquals("Status code", expectedStatus, response.getStatusLine().getStatusCode());
+	}
+
+	private void get(String path) throws IOException, URISyntaxException {
+		URI url = new URI("http://localhost:" + APPLICATION_PORT + path);
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		this.response = httpClient.execute(new HttpGet(url));
+	}
 
 	@BeforeClass
 	public static void startApplication() throws Exception {
@@ -23,8 +56,8 @@ public class HangmanEnd2EndTest {
 		app.shutdown();
 	}
 
-	@AfterClass
-	public static void closeDriver() throws Exception {
-		driver.close();
-	}
+	private static final int APPLICATION_PORT = 8123;
+	protected static ReusableJettyApp app = new ReusableJettyApp(new HangmanServlet());
+	private HttpResponse response;
+
 }
--- hangman/hangman-server/src/main/java/it/xpug/hangman/main/HangmanServlet.java
+++ hangman/hangman-server/src/main/java/it/xpug/hangman/main/HangmanServlet.java
@@ -1,7 +1,16 @@
 package it.xpug.hangman.main;
 
+import java.io.*;
+
+import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class HangmanServlet extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write("{}");
+	}
 }
