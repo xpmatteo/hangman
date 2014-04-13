ca39a29dee9a6defd0c6578f042ae5eb5073d5a8
Matteo Vaccari <vaccari@pobox.com>
2014-04-13 11:57:33 +0200

    Removed more duplication

--- src/test/java/it/xpug/hangman/main/UserControllerTest.java
+++ src/test/java/it/xpug/hangman/main/UserControllerTest.java
@@ -22,12 +22,9 @@ public class UserControllerTest {
 
 	@Test
 	public void itTakesANameToCreateAUser() throws Exception {
-		UserBase users = new UserBase();
-		UserController controller = new UserController(users);
-
 		givenPostRequest("/users");
+		givenNoParameters();
 		context.checking(new Expectations() {{
-			allowing(request).getParameter(with(any(String.class))); will(returnValue(null));
 			oneOf(response).validationError("Parameter 'name' is required");
 		}});
 
@@ -36,41 +33,23 @@ public class UserControllerTest {
 
 	@Test
 	public void itTakesAPasswordToCreateAUser() throws Exception {
-		UserBase users = new UserBase();
-		UserController controller = new UserController(users);
-
 		givenPostRequest("/users");
+		givenParameter("name", "gino");
+		givenNoParameter("password");
 		context.checking(new Expectations() {{
-			allowing(request).getParameter(with("name")); will(returnValue("gino"));
-			allowing(request).getParameter(with(any(String.class))); will(returnValue(null));
 			oneOf(response).validationError("Parameter 'password' is required");
 		}});
 
 		controller.handleRequest(request, response);
 	}
 
-	private void givenPostRequest(final String path) {
-		context.checking(new Expectations() {{
-			allowing(request).getRequestPath(); will(returnValue(path));
-			allowing(request).isGet(); will(returnValue(false));
-			allowing(request).isPost(); will(returnValue(true));
-		}});
-	}
-
-	private void givenGetRequest() {
-		context.checking(new Expectations() {{
-			allowing(request).isGet(); will(returnValue(true));
-			allowing(request).isPost(); will(returnValue(false));
-		}});
-	}
-
 	@Test
 	public void createsAUser() throws Exception {
-
 		givenPostRequest("/users");
+		givenParameter("name", "gino");
+		givenParameter("password", "secr3t");
+
 		context.checking(new Expectations() {{
-			allowing(request).getParameter(with("name")); will(returnValue("gino"));
-			allowing(request).getParameter(with("password")); will(returnValue("secr3t"));
 			oneOf(response).created("/users/3cb54a30");
 		}});
 
@@ -83,12 +62,11 @@ public class UserControllerTest {
 	public void returnListOfPrisoners() throws Exception {
 		final UserId userId = new UserId("1234");
 		users.add(userId, "name", "s3cret");
-		givenGetRequest();
+		givenGetRequest("/users/1234/prisoners");
+		givenParameter("password", "s3cret");
 		context.checking(new Expectations() {{
-			allowing(request).getRequestPath(); will(returnValue("/users/1234/prisoners"));
-			allowing(request).getParameter(with("password")); will(returnValue("s3cret"));
 			allowing(request).getUserId(); will(returnValue(userId));
-			oneOf(response).put("items", new ArrayList<Prisoner>());
+			oneOf(response).put("items", emptyList());
 			oneOf(response).put("url", "/users/1234/prisoners");
 		}});
 		controller.handleRequest(request, response);
@@ -99,8 +77,8 @@ public class UserControllerTest {
 		final UserId userId = new UserId("1234");
 		users.add(userId, "name", "s3cret");
 		givenPostRequest("/users/1234/prisoners");
+		givenParameter("password", "s3cret");
 		context.checking(new Expectations() {{
-			allowing(request).getParameter(with("password")); will(returnValue("s3cret"));
 			allowing(request).getUserId(); will(returnValue(userId));
 			oneOf(response).created("/users/1234/prisoners/3cb54a30");
 		}});
@@ -116,9 +94,10 @@ public class UserControllerTest {
 		users.addPrisoner(userId, new Prisoner("abc123", "word"));
 
 		givenPostRequest("/users/1234/prisoners/abc123");
+		givenParameter("password", "s3cret");
+		givenParameter("guess", "x");
+
 		context.checking(new Expectations() {{
-			allowing(request).getParameter(with("password")); will(returnValue("s3cret"));
-			allowing(request).getParameter(with("guess")); will(returnValue("x"));
 			allowing(request).getUserId(); will(returnValue(userId));
 			allowing(request).getPrisonerId(); will(returnValue("abc123"));
 			oneOf(response).created("/users/1234/prisoners/abc123");
@@ -135,9 +114,9 @@ public class UserControllerTest {
 		users.addPrisoner(userId, new Prisoner("abc123", "word"));
 
 		givenPostRequest("/users/1234/prisoners/abc123");
+		givenParameter("password", "s3cret");
+		givenNoParameter("guess");
 		context.checking(new Expectations() {{
-			allowing(request).getParameter(with("password")); will(returnValue("s3cret"));
-			allowing(request).getParameter(with("guess")); will(returnValue(null));
 			allowing(request).getUserId(); will(returnValue(userId));
 			oneOf(response).validationError("Parameter 'guess' is required");
 		}});
@@ -153,9 +132,9 @@ public class UserControllerTest {
 		users.addPrisoner(userId, new Prisoner("abc123", "word"));
 
 		givenPostRequest("/users/1234/prisoners/abc123");
+		givenNoParameter("password");
+		givenParameter("guess", "x");
 		context.checking(new Expectations() {{
-			allowing(request).getParameter(with("password")); will(returnValue(null));
-			allowing(request).getParameter(with("guess")); will(returnValue("x"));
 			allowing(request).getUserId(); will(returnValue(userId));
 			allowing(request).getPrisonerId(); will(returnValue("abc123"));
 			oneOf(response).forbidden(with(any(String.class)));
@@ -165,4 +144,41 @@ public class UserControllerTest {
 		assertEquals(18, users.findPrisoner(userId, "abc123").getGuessesRemaining());
 	}
 
+	private void givenPostRequest(final String path) {
+		context.checking(new Expectations() {{
+			allowing(request).getRequestPath(); will(returnValue(path));
+			allowing(request).isGet(); will(returnValue(false));
+			allowing(request).isPost(); will(returnValue(true));
+		}});
+	}
+
+	private void givenGetRequest(final String path) {
+		context.checking(new Expectations() {{
+			allowing(request).getRequestPath(); will(returnValue(path));
+			allowing(request).isGet(); will(returnValue(true));
+			allowing(request).isPost(); will(returnValue(false));
+		}});
+	}
+
+	private void givenNoParameters() {
+		context.checking(new Expectations() {{
+			allowing(request).getParameter(with(any(String.class))); will(returnValue(null));
+		}});
+	}
+
+	private void givenParameter(final String name, final String value) {
+		context.checking(new Expectations() {{
+			allowing(request).getParameter(with(name)); will(returnValue(value));
+		}});
+	}
+
+	private void givenNoParameter(final String name) {
+		context.checking(new Expectations() {{
+			allowing(request).getParameter(name); will(returnValue(null));
+		}});
+	}
+
+	private List<Prisoner> emptyList() {
+		return new ArrayList<Prisoner>();
+	}
 }
